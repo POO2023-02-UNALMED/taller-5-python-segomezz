cc53e876005b2b2d9285c56f9225a55d88223268
segomezz <141938389+segomezz@users.noreply.github.com>
2023-09-24 19:07:54 -0500

    Update anfibio.py

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -1,5 +1,6 @@
 from .animal import Animal
 class Anfibio(Animal):
+    _listado=[]
     ranas=0
     salamandras=0
     def __init__(self, nombre = "", edad = 0, habitat = "", genero = "", colorPiel = "", venenoso = False):
